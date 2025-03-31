48562b611dcffa0bdb74b902a83df00352a7877f
Spie1er <berulava@gmail.com>
2025-03-31 13:43:16 +0400

    add auth actions for sign up, sign in, cookie sessions.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
+import InterviewCard from '@/components/InterviewCard'
 
 const HomePage = () => {
-    return (
-        <div>HomePage</div>
-    )
+  return (
+    <>
+      {/*Hero Section*/}
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-w-lg'>
+          <h2>Simulate AI-Powered Professional Interviews and Get Feedback</h2>
+          <p className='text-lg'>
+            Practice and be ready to land your dream job
+          </p>
+
+          <Button asChild className='btn-primary max-sm:w-full'>
+            <Link href='/interview'>Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src='/robot.png'
+          alt='robot'
+          width={400}
+          height={400}
+          className='max-md:hidden'
+        />
+      </section>
+
+      {/*Your Interviews Section*/}
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Your Interviews</h2>
+        <div className='interviews-section'>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              {...interview}
+              key={`yourInterview-${interview.id}`}
+            />
+          ))}
+          {/*<p>You haven't taken any interviews yet</p>*/}
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-11'>
+        <h2>Take an Interview</h2>
+        <div className='interviews-section'>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              {...interview}
+              key={`otherInterviews-${interview.id}`}
+            />
+          ))}
+          {/*<p>There are no interviews available</p>*/}
+        </div>
+      </section>
+    </>
+  )
 }
 export default HomePage
---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCePLP24ZHA3IhqCx1VYFEE2BMvSWtHwoA',
+  authDomain: 'interv-u.firebaseapp.com',
+  projectId: 'interv-u',
+  storageBucket: 'interv-u.firebasestorage.app',
+  messagingSenderId: '126606380612',
+  appId: '1:126606380612:web:f11f4795daaeedd23f3611',
+  measurementId: 'G-YBMW6TDLWQ'
+}
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp()
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
