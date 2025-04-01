51daa46491bcf010c0290204c785c14c84cec4cc
Spie1er <berulava@gmail.com>
2025-04-01 14:54:19 +0400

    change env file and firebase client config

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CLIENT_API_KEY,
-  authDomain: 'interv-u.firebaseapp.com',
-  projectId: 'interv-u',
-  storageBucket: 'interv-u.firebasestorage.app',
-  messagingSenderId: '126606380612',
-  appId: '1:126606380612:web:f11f4795daaeedd23f3611',
-  measurementId: 'G-YBMW6TDLWQ'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
